ef474bb8f3bb2262b9a6fef36ebcbe8b1a355024
dmcdaniel90 <dmcdaniel9@gmail.com>
2023-11-14 12:01:33 +0000

    initial commit, created components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  num: 10,
+  type: 'circle',
+  bg: true
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className='App'>
+        <ParticlesBg {...particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* {
+        <FaceRecogniton />} */}
+      </div>
+    )
+  }
 }
 
 export default App;
