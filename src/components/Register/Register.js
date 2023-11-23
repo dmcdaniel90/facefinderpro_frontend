c21997dcc2ac4f2dcb1c3008892e34f54d3aaa8f
dmcdaniel90 <dmcdaniel9@gmail.com>
2023-11-23 14:31:59 +0000

    Added session storage feature

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,10 +3,10 @@ import React from "react";
 
 const Register = ({onRouteChange}) => {
   return (
-    <article className="br3 ba dark-gray b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-      <main className="pa4 white-80">
-        <form className="measure">
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+      <article className="br3 ba dark-grey b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 white-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f2 fw6 ph0 mh0 center">Register</legend>
             <div className="mt3">
               <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
