48591af06027df6b914190da80c47d996d1b49ad
Devin McDaniel <devinmcdaniel@Devins-MacBook-Pro.local>
2025-03-14 16:19:26 +0000

    testing

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -23,6 +23,8 @@ export default function Register() {
   const onSubmitSignIn = async (e) => {
     e.preventDefault();
 
+    console.log(import.meta.env.VITE_BACKEND_URL);
+
     try {
       const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/register`, {
         method: "post",
